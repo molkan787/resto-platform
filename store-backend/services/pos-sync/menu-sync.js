ec8cc74c400f6a13a26995ae190cf1c0c9af5a97
Dahmane Seghier <dahmane.seghier@gmail.com>
2021-02-24 18:04:05 +0100

    Added offers syncing

--- store-backend/services/pos-sync/menu-sync.js
+++ store-backend/services/pos-sync/menu-sync.js
@@ -1,13 +1,41 @@
 const mongoose = require('mongoose');
+const { OfferGetType } = require('murew-core/dist/interfaces/Offer');
+const { off } = require('strapi-utils/lib/logger');
 
 module.exports = class MurewMenuSync{
 
     static async setMenu(store_id, data){
-        const { menu } = data;
+        const { menu, offers } = data;
         const queries = menu.map(cat => this.upsertCategory(cat, store_id));
         const response = await Promise.all(queries);
         const categories = [].concat(...response.map(d => d.categories));
         const products = [].concat(...response.map(d => d.products));
+
+        for(let offer of offers){
+            const { benefits: [benefit] } = offer;
+            if(benefit.type == OfferGetType.FreeItems){
+                if(benefit.all_items){
+                    benefit.items = [];
+                }else{
+                    const items = await strapi.query('product').find({
+                        remote_id_in: benefit.items
+                    });
+                    benefit.items = items.map(item => item.id);
+                }
+            }
+            offer.published_at = new Date();
+            offer.store_id = store_id;
+            offer.remote_id = offer.id;
+            delete offer.id;
+            ['available_on_delivery',
+            'available_on_pickup',
+            'available_on_website',
+            'available_on_pos',
+            'activated_by_promo_code']
+            .forEach(prop => offer[prop] = !!offer[prop]);
+        }
+
+        const offersIds = await Promise.all(offers.map(offer => this.upsertOffer(offer)));
 
         await strapi.query('category').delete({
             store_id,
@@ -20,6 +48,25 @@ module.exports = class MurewMenuSync{
             id_nin: products,
             remote_id_null: false
         });
+        
+        await strapi.query('offer').delete({
+            store_id,
+            id_nin: offersIds,
+            remote_id_null: false
+        });
+
+    }
+
+    static async upsertOffer(offer){
+        const query = strapi.query('offer');
+        const entry = await query.findOne({ remote_id: offer.remote_id });
+        if(entry){
+            await query.update({ id: entry.id }, offer);
+            return entry.id;
+        }else{
+            const { id } = await query.create(offer);
+            return id;
+        }
     }
 
     static async upsertCategory(category, store_id, parentId){
