1d44972395e27f83a502213923b5a959bf9c3990
Dahmane Seghier <dahmane.seghier@gmail.com>
2021-07-25 17:39:07 +0100

    Disabled transactional email sending

--- store-backend/api/email/services/email.js
+++ store-backend/api/email/services/email.js
@@ -1,32 +1,33 @@
 // TEMPORARY SOLUTION
 // TODO: create email sending app (separate from store backend app)
 
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 
-const ACCOUNT = Object.freeze({
-    Address: 'noreply@drmapps.eu',
-    Password: '6utdPagdxfy901t4PiuR'
-})
+// const ACCOUNT = Object.freeze({
+//     Address: 'noreply@drmapps.eu',
+//     Password: '6utdPagdxfy901t4PiuR'
+// })
 
-const transport = nodemailer.createTransport({
-    host: 'server343197.nazwa.pl',
-    port: 587,
-    auth: {
-        user: ACCOUNT.Address,
-        pass: ACCOUNT.Password
-    }
-})
+// const transport = nodemailer.createTransport({
+//     host: 'server343197.nazwa.pl',
+//     port: 587,
+//     auth: {
+//         user: ACCOUNT.Address,
+//         pass: ACCOUNT.Password
+//     }
+// })
 
 module.exports = {
 
     send(to, subject, content, isHTML){
-        return transport.sendMail({
-            from: `Murew <${ACCOUNT.Address}>`,
-            to: to,
-            subject: subject,
-            text: isHTML ? undefined : content,
-            html: isHTML ? content : undefined,
-        })
+        return Promise.resolve();
+        // return transport.sendMail({
+        //     from: `Murew <${ACCOUNT.Address}>`,
+        //     to: to,
+        //     subject: subject,
+        //     text: isHTML ? undefined : content,
+        //     html: isHTML ? content : undefined,
+        // })
     }
 
 }
