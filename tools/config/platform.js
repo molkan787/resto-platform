dbd6fdf5ffd142c36152fb46a35cf0b5d19b187a
Seghier Dahmane <worw787@gmail.com>
2024-01-18 09:54:50 +0000

    remove unused files

--- tools/config/platform.js
+++
@@ -1,8 +0,0 @@
-const PLATFORM_PROD_DIR = '/var/murew';
-const PLATFORM_APPS = [
-    'supervisor',
-    'murew-backend',
-    'reverseproxy'
-];
-
-module.exports = { PLATFORM_PROD_DIR, PLATFORM_APPS };
