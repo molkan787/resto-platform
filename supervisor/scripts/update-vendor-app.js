0eb1a1f79e71b3e99bcfb74f89cd4b357f6a211b
Seghier Dahmane <worw787@gmail.com>
2024-01-28 15:09:32 +0100

    supervisor: updated vendor app updated script

--- supervisor/scripts/update-vendor-app.js
+++ supervisor/scripts/update-vendor-app.js
@@ -9,11 +9,26 @@ async function run(){
     const mongoClient = await MongoClient.connect(DATABASE_URI, { useUnifiedTopology: true });
     const db = mongoClient.db(MASTER_BACKEND_DB_NAME);
     const vendorDoc = await db.collection('vendors').findOne({ _id: ObjectId(vendorId) })
+    await fillComponentsData(db, vendorDoc)
     vendorDoc.id = vendorDoc._id.toString()
+    if(process.env.LOG_LEVEL === 'verbose'){
+        console.log('vendorDoc:', vendorDoc)
+    }
     console.log(`Updating vendor "${vendorDoc.Name}" with domain "${vendorDoc.domain}"`)
     await sleep(10000) // Giving time to user to cancel the operation if needed
     await axios.post('http://127.0.0.1:1323/update-vendor-app', { app: vendorDoc })
     console.log('Successfully updated vendor app!')
+}
+
+async function fillComponentsData(db, vendorDoc){
+    const featuresComp = (vendorDoc.features || [])[0] || {}
+    if(featuresComp.kind === 'ComponentDivertPlatformFeatures'){
+        const coll = db.collection('components_divert_platform_features')
+        const fcDoc = await coll.findOne({ _id: ObjectId(featuresComp.ref) })
+        if(typeof fcDoc === 'object' && fcDoc !== null){
+            vendorDoc.features = fcDoc
+        }
+    }
 }
 
 run()
