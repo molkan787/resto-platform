c12f6fa59919d982d6149c809346831b25e5df3a
Seghier Dahmane <worw787@gmail.com>
2024-01-28 09:56:20 +0100

    updated vendor app entry point to start storefront only if an env varible is present with certain value

--- run-all.sh
+++ run-all.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Enable
-set -m
-  
-# Start the frontend server
-yarn --cwd store-frontend start &
-  
-# Start the backend server
-yarn --cwd store-backend start
+if [ "$ENABLE_STOREFRONT" == "yes" ]
+then
+    # Enable
+    set -m
+    # Start the frontend server
+    yarn --cwd store-frontend start &
+    # Start the backend server
+    yarn --cwd store-backend start
+else
+    # Start the backend server
+    yarn --cwd store-backend start
+fi
