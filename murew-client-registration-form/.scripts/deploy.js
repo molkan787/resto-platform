f48bcdc6050be58a7eab7f2a04148e384e48d32c
Seghier Dahmane <worw787@gmail.com>
2024-03-04 17:33:31 +0100

    murew-client-registration-form: added deployment script

---
+++ murew-client-registration-form/.scripts/deploy.js
@@ -0,0 +1,37 @@
+const child_process = require('child_process');
+
+/**
+ * 
+ * @param {string} cmd 
+ * @param {{ encoding: "buffer" | null; } & child_process.ExecOptions} options 
+ * @returns 
+ */
+function exec(cmd, options){
+    return new Promise((resolve, reject) => {
+        child_process.exec(cmd, options, (error, stdout, stderr) => {
+            if(error) reject(error)
+            else resolve({ stdout, stderr })
+        });
+    });
+}
+
+async function run(){
+    console.log('Building app...')
+    await exec('yarn build', { cwd: '~/resto-e-commerce/murew-client-registration-form' })
+    console.log('Clearing previously deployed files...')
+    await Promise.all([
+        exec('rm -rf ~/murew-presentation-website/registration-form-app/assets'),
+        exec('rm ~/murew-presentation-website/registration-form-app/favicon.ico'),
+        exec('rm ~/murew-presentation-website/registration-form-app/index.html'),
+    ])
+    console.log('Copying new files...')
+    await exec('cp ~/resto-e-commerce/murew-client-registration-form/dist/* ~/murew-presentation-website/registration-form-app/')
+}
+
+
+run()
+.then(() => console.log('Deployment Completed.'))
+.catch(err => {
+    console.error('DEPLOYMENT FAILED')
+    console.error(err)
+})
