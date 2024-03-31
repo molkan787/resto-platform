a326bdd437c48a1b5037e318e0a647cfef456ce5
Seghier Dahmane <worw787@gmail.com>
2024-03-31 01:44:26 +0100

    store-frontend: adjusted footer layout and added powered by DFT tag (info link)

--- store-frontend/components/common/Footer.vue
+++ store-frontend/components/common/Footer.vue
@@ -1,31 +1,39 @@
 <template>
     <div class="footer">
         <div class="content">
-            <nuxt-link to="/">HOME</nuxt-link>
+            <div class="col">
+                <nuxt-link to="/">HOME</nuxt-link>
 
-            <template v-if="stores.length == 1">
-                <nuxt-link :to="`/order/${stores[0].slug}`">ORDER</nuxt-link>
-            </template>
-            <template v-else>
-                <nuxt-link v-for="store in stores" :key="store.slug" :to="'/order/' + store.slug">
-                    ORDER ON {{ store.name | uppercase }}
+                <template v-if="stores.length == 1">
+                    <nuxt-link :to="`/order/${stores[0].slug}`">ORDER</nuxt-link>
+                </template>
+                <template v-else>
+                    <nuxt-link v-for="store in stores" :key="store.slug" :to="'/order/' + store.slug">
+                        ORDER ON {{ store.name | uppercase }}
+                    </nuxt-link>
+                </template>
+
+                <template v-if="bookableStores.length == 1">
+                    <nuxt-link :to="`/book/${bookableStores[0].slug}`">BOOK TABLE</nuxt-link>
+                </template>
+                <template v-else-if="bookableStores.length > 1">
+                    <nuxt-link v-for="store in bookableStores" :key="store.slug" :to="'/book/' + store.slug">
+                        BOOK TABLE IN {{ store.name | uppercase }}
+                    </nuxt-link>
+                </template>
+
+                <nuxt-link to="/gallery">GALLERY</nuxt-link>
+
+            </div>
+            <div class="col">
+                <nuxt-link v-for="page in pages" :key="page.slug" :to="'/p/' + page.slug">
+                    {{ page.name | uppercase }}
                 </nuxt-link>
-            </template>
-
-            <template v-if="bookableStores.length == 1">
-                <nuxt-link :to="`/book/${bookableStores[0].slug}`">BOOK TABLE</nuxt-link>
-            </template>
-            <template v-else-if="bookableStores.length > 1">
-                <nuxt-link v-for="store in bookableStores" :key="store.slug" :to="'/book/' + store.slug">
-                    BOOK TABLE IN {{ store.name | uppercase }}
-                </nuxt-link>
-            </template>
-
-            <nuxt-link to="/gallery">GALLERY</nuxt-link>
-
-            <nuxt-link v-for="page in pages" :key="page.slug" :to="'/p/' + page.slug">
-                {{ page.name | uppercase }}
-            </nuxt-link>
+            </div>
+            <div class="col">
+                Powered by <br>
+                <a href="https://digitalfoodtechnology.com" target="_blank">Digital Food Technology</a>
+            </div>
         </div>
     </div>
 </template>
@@ -45,20 +53,22 @@ export default {
 
 <style lang="scss" scoped>
 .footer {
-    width: 100%;
     padding: 2rem;
+    width: 100%;
     background-color: rgba(var(--vs-primary), 0.1);
     border-bottom: 4px solid rgba(var(--vs-primary), 1);
-    .content {
+    .content{
         max-width: 1000px;
         width: 100%;
         margin: auto;
-        display: grid;
-        grid-template-columns: repeat(4, calc(100% / 4));
-        gap: 10px;
-        @media (max-width: 768px) {
-            &{
-                grid-template-columns: repeat(2, calc(100% / 2));
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        .col{
+            display: flex;
+            flex-direction: column;
+            a{
+                margin-bottom: 0.5rem;
             }
         }
     }
